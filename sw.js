e9b18d24c53839e9ebe17e70cf686303d6ba4f22
annatndp <anna.tndp@gmail.com>
2026-08-01 18:58:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "idea-vault-v1";
-const SHELL = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+const CACHE = "idea-vault-v2";
+const SHELL = ["./","./index.html","./manifest.webmanifest","./config.js","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
